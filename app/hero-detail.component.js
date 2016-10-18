594377aad71352e4ff06513e20f714f7d60ddb2f
Travis Baker <baker.travis.w@gmail.com>
2016-10-17 22:12:07 -0500

    https://angular.io/docs/ts/latest/tutorial/toh-pt3.html

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -8,25 +8,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var forms_1 = require('@angular/forms');
-var app_component_1 = require('./app.component');
-var AppModule = (function () {
-    function AppModule() {
+var core_1 = require("@angular/core");
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                forms_1.FormsModule
-            ],
-            declarations: [app_component_1.AppComponent],
-            bootstrap: [app_component_1.AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    return HeroDetailComponent;
 }());
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+HeroDetailComponent = __decorate([
+    core_1.Component({
+        selector: 'my-hero-detail',
+    }),
+    __metadata("design:paramtypes", [])
+], HeroDetailComponent);
+exports.HeroDetailComponent = HeroDetailComponent;
+//# sourceMappingURL=hero-detail.component.js.map
